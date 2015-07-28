e044cdc2f1c3879aee6ad5f745ed16262e629221
Matt <mdlkwd@mst.edu>
2015-07-27 20:38:50 -0500

    toLong is only performed once.

--- src/Problem6.scala
+++ src/Problem6.scala
@@ -1,6 +1,6 @@
 object Problem6 extends EulerSolver {
-  val sumOfSquares = (1 to 100).map(i => Math.pow(i, 2)).sum.toLong
-  val squareOfTheSum = Math.pow((1 to 100).sum, 2).toLong
+  val sumOfSquares = (1 to 100).map(i => Math.pow(i, 2)).sum
+  val squareOfTheSum = Math.pow((1 to 100).sum, 2)
 
-  val answer = squareOfTheSum - sumOfSquares
+  val answer = (squareOfTheSum - sumOfSquares).toLong
 }
